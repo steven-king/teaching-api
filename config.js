947b5a13af4411bba9904853733c191f5c5849bf
steven-king <steven.king@unc.edu>
2019-11-12 20:11:54 -0500

    adding config

---
+++ config.js
@@ -0,0 +1,8 @@
+// in your terminal, write > cp config.default.js config.js and change the following values
+// get these keys in https://apps.twitter.com/app/new
+module.exports = {
+  consumer_key: 'h9nWpPMflUYJWVHprs9GXDws1',
+  consumer_secret: 'vTxDeC5qnY6rU5B0nH06v6tN7erkNHgMyysAjkwrPwTWxfX9cW',
+  access_token_key: '16192753-McJ0xYDzEBeEuCnaK5V1tf54fuTqkZ8LoggPZXN1s',
+  access_token_secret: 'hyRycMMVbWlNgrDEyQAjfKUZtEnzRIgnZEbus8amLTYgr'
+}
