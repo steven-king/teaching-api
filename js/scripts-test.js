1504ff105c1c0519208034131cab866b3702c702
steven-king <steven.king@unc.edu>
2019-11-18 00:03:46 -0500

    drop www

--- js/scripts-test.js
+++ js/scripts-test.js
@@ -9,7 +9,7 @@ function loadData(){
 console.log('loadData')
     $.ajax({
             type:"GET",
-            url:"https://www.storage.cloud.google.com/teaching-api/tweets.json",
+            url:"https://storage.cloud.google.com/teaching-api/tweets.json",
             dataType:"json",
             headers: {
                 'Access-Control-Allow-Origin': '*'
